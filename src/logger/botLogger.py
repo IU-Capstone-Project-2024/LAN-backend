76882fb4f485e201a760f8ed8110a86accb13104
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2024-06-28 11:22:37 +0300

    add

--- src/logger/botLogger.py
+++ src/logger/botLogger.py
@@ -4,8 +4,7 @@ from pathlib import Path
 botLogger = logging.getLogger(__name__)
 botLogger.setLevel(logging.DEBUG)
 
-logFile = Path(__file__).parent.parent / "logs/bot.log"
-print(logFile)
+logFile = Path(__file__).parent.parent.parent / "logs/bot.log"
 fileHandler = logging.FileHandler(logFile)
 
 dateformat = '%Y-%m-%d %H:%M:%S'
